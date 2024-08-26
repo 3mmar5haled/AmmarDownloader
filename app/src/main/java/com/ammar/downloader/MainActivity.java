6302ec96aa0775174b074ee0732e320ff62993c9
Ammar Khaled <3mmar7haled@gmail.com>
2024-08-26 13:01:51 +0300

    Push Project

---
+++ app/src/main/java/com/ammar/downloader/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ammar.downloader;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.ammar.downloader.databinding.ActivityMainBinding;
+
+public class MainActivity extends Activity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        this.binding = null;
+    }
+}
